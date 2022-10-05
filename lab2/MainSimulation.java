72d603650f4d4ac628c13b8407268497becabd04
Kristjan Jonsson <pe1178jo-s@student.lu.se>
2022-10-05 22:37:00 +0200

    1.11

--- lab2/MainSimulation.java
+++ lab2/MainSimulation.java
@@ -13,55 +13,57 @@ public class MainSimulation extends Global{
     	Signal actSignal;
     	new SignalList();
 
-		Map<String, QS> qMap = new TreeMap<String, QS>();
+		List<QS> qList = new ArrayList<QS>();
 		for (int i = 0; i < queuesNbr; i++) {
-			qMap.put("Q"+(1+i), new QS());
+			qList.add(i, new QS());
 		}
-
-		List<Map.Entry<String, QS>> qList = new ArrayList<>(qMap.entrySet());
 
 		SignalList.SendSignal(READY, Generator, time);
 		for (int i = 0; i < queuesNbr; i++) {
-			SignalList.SendSignal(MEASURE, qList.get(i).getValue(), time);
+			SignalList.SendSignal(MEASURE, qList.get(i), time);
 		}
 
 		while (time < 100000){
-			Generator.sendTo = qList.get(0).getValue();
+			Generator.sendTo = qList.get(0);
     		actSignal = SignalList.FetchSignal();
     		time = actSignal.arrivalTime;
     		actSignal.destination.TreatSignal(actSignal);
-			qList.sort((q1, q2) -> (q1.getValue().numberInQueue - q2.getValue().numberInQueue));
+			qList.sort((q1, q2) -> (q1.numberInQueue - q2.numberInQueue));
     	} 
-
+		
 		System.out.println();
 		for (int i = 0; i < queuesNbr; i++) {
-			System.out.println("Medelantalet kunder i kösystem " + (i+1) + ": " + 1.0*qList.get(i).getValue().accumulated/qList.get(i).getValue().noMeasurements);
+			System.out.println("Medelantalet kunder i kösystem " + (i+1) + ": " + 1.0*qList.get(i).accumulated/qList.get(i).noMeasurements);
 		}
 		System.out.println();
 
 
-		/** UPPGIFT 1e) 
+		
+
+
+
+		/** UPPGIFT 1e) FÖRST EN SEN EN ANNAN SEN EN ANNAN I KEDJA
 		 * 		Generator.lambda = 8;
 		 *		int queuesNbr = 3;
-		Generator.sendTo = qList.get(0).getValue();
-		qList.get(0).getValue().sendTo = qList.get(1).getValue();
-		qList.get(1).getValue().sendTo = qList.get(2).getValue();
+		Generator.sendTo = qList.get(0);
+		qList.get(0).sendTo = qList.get(1);
+		qList.get(1).sendTo = qList.get(2);
 
 		*/
 
 		/** KÖSYSTEM VÄLJS PÅ SLUMP
 		Random rand = new Random();
 		while (time < 100000){
-			Generator.sendTo = qList.get(rand.nextInt(5)).getValue();
+			Generator.sendTo = qList.get(rand.nextInt(5));
     		actSignal = SignalList.FetchSignal();
     		time = actSignal.arrivalTime;
     		actSignal.destination.TreatSignal(actSignal);
-    	}  */
+    	} */
 
 		/** KÖSYSTEM VÄLJS EN EFTER EN
 		int q = 0;
 		while (time < 100000){
-			Generator.sendTo = qList.get(q).getValue();
+			Generator.sendTo = qList.get(q);
     		actSignal = SignalList.FetchSignal();
     		time = actSignal.arrivalTime;
     		actSignal.destination.TreatSignal(actSignal);
@@ -71,14 +73,13 @@ public class MainSimulation extends Global{
 			}
     	}  */
 
-		
 		/** KÖSYSTEM VÄLJS EFTER DEN SOM HAR MINST JOBB
 		while (time < 100000){
-			Generator.sendTo = qList.get(0).getValue();
+			Generator.sendTo = qList.get(0);
     		actSignal = SignalList.FetchSignal();
     		time = actSignal.arrivalTime;
     		actSignal.destination.TreatSignal(actSignal);
-			qList.sort((q1, q2) -> (q1.getValue().numberInQueue - q2.getValue().numberInQueue));
+			qList.sort((q1, q2) -> (q1.numberInQueue - q2.numberInQueue));
     	} */
 
 
